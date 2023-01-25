329ce95a3b08d37564d48152613ad0c3e8966afd
Guilherme Taveira <gb-ol@hotmail.com>
2023-01-24 23:38:14 -0300

    Adicionando Interfaces IPieRepository e ICategoryRepository

---
+++ BethanysPieShop/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using BethanysPieShop.Models;
+
+namespace BethanysPieShop.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> AllCategories { get; }
+    }
+}
---
+++ BethanysPieShop/Interfaces/IPieRepository.cs
@@ -0,0 +1,11 @@
+﻿using BethanysPieShop.Models;
+
+namespace BethanysPieShop.Interfaces
+{
+    public interface IPieRepository
+    {
+        IEnumerable<Pie> AllPies { get; }
+        IEnumerable<Pie> PiesOfTheWeek { get; }
+        Pie? GetPieById(int pieId);
+    }
+}
