37918f836475ea94f89ac200fb1b65fae0c446eb
Guilherme Taveira <gb-ol@hotmail.com>
2023-01-25 16:36:07 -0300

    Adicionando Repositories CategoryRepository e PieRepository

---
+++ BethanysPieShop/Repositories/CategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using BethanysPieShop.Context;
+using BethanysPieShop.Interfaces;
+using BethanysPieShop.Models;
+
+namespace BethanysPieShop.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly BethanysPieShopDbContext _bethanysPieShopDbContext;
+
+        public CategoryRepository(BethanysPieShopDbContext bethanysPieShopDbContext)
+        {
+            _bethanysPieShopDbContext = bethanysPieShopDbContext;
+        }
+
+        public IEnumerable<Category> AllCategories => _bethanysPieShopDbContext.Categories.OrderBy(c => c.CategoryName);
+    }
+}
