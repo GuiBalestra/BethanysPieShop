5dafc35966ffd24c1b3a4a1ac3e3850f50db3f6c
Guilherme Taveira <gb-ol@hotmail.com>
2023-01-25 15:07:38 -0300

    Adicionando Context para Categories e Pies

---
+++ BethanysPieShop/Context/BethanysPieShopDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BethanysPieShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BethanysPieShop.Context
+{
+    public class BethanysPieShopDbContext : DbContext
+    {
+        public BethanysPieShopDbContext(DbContextOptions<BethanysPieShopDbContext> options) : base(options) { }
+        
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Pie> Pies { get; set; }
+    }
+}
