28a2d3159e9bdb4b356afb489795c175f7f699b6
Guilherme Taveira <gb-ol@hotmail.com>
2023-01-26 10:45:00 -0300

    Adicionando Interface IShoppingCart

---
+++ BethanysPieShop/Interfaces/IShoppingCart.cs
@@ -0,0 +1,14 @@
+﻿using BethanysPieShop.Models;
+
+namespace BethanysPieShop.Interfaces
+{
+    public interface IShoppingCart
+    {
+        void AddToCart(Pie pie);
+        int RemoveFromCart(Pie pie);
+        List<ShoppingCartItem> GetShoppingCartItems();
+        void ClearCart();
+        decimal GetShoppingCartTotal();
+        List<ShoppingCartItem> ShoppingCartItems { get; set; }
+    }
+}
