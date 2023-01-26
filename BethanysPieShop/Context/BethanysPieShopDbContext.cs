8f9997e3f1da3c0d653b5dd031ef4176dc35a1c7
Guilherme Taveira <gb-ol@hotmail.com>
2023-01-26 10:44:27 -0300

    Adicionando Context ShoppingCartItems

--- BethanysPieShop/Context/BethanysPieShopDbContext.cs
+++ BethanysPieShop/Context/BethanysPieShopDbContext.cs
@@ -9,5 +9,6 @@ namespace BethanysPieShop.Context
         
         public DbSet<Category> Categories { get; set; }
         public DbSet<Pie> Pies { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
     }
 }
